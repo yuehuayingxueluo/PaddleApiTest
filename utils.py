d0da10a25daa6bd6643de38d49295ddce5cf8f72
Charles-hit <wanghao107@baidu.com>
2023-04-07 06:42:10 +0000

    add matmul and softmax test

---
+++ utils.py
@@ -0,0 +1,16 @@
+import torch
+
+TOLERANCE = {
+    "float32": {"atol": 1e-6, "rtol": 1e-6},
+    "float16": {"atol": 1e-4, "rtol": 1e-4},
+    "bfloat16": {"atol": 1e-3, "rtol": 1e-3},
+}
+
+def convert_dtype_to_torch_type(dtype):
+    if dtype == 'float32':
+        return torch.float32
+    elif dtype == 'float16':
+        return torch.float16
+    elif dtype == 'bfloat16':
+        return torch.bfloat16
+    
