fix memory overflow

test_matmul/test_matmul_develop.py
@@ -9,17 +9,17 @@ from utils import TOLERANCE, convert_dtype_to_torch_type
 
 
 def generate_np_inputs_and_dout():
-    x_case1 = np.random.random(size=[1, 32, 4096, 192])
-    y_case1 = np.random.random(size=[1, 32, 4096, 192])
-    dout_case1 = np.random.random(size=[1, 32, 4096, 4096])
+    x_case1 = np.random.random(size=[1, 32, 4096, 192]).astype("float32")
+    y_case1 = np.random.random(size=[1, 32, 4096, 192]).astype("float32")
+    dout_case1 = np.random.random(size=[1, 32, 4096, 4096]).astype("float32")
 
-    x_case2 = np.random.random(size=[1, 32, 4096, 4096])
-    y_case2 = np.random.random(size=[1, 32, 4096, 4096])
-    dout_case2 = np.random.random(size=[1, 32, 4096, 4096])
+    x_case2 = np.random.random(size=[1, 32, 4096, 4096]).astype("float32")
+    y_case2 = np.random.random(size=[1, 32, 4096, 4096]).astype("float32")
+    dout_case2 = np.random.random(size=[1, 32, 4096, 4096]).astype("float32")
 
-    x_case3 = np.random.random(size=[1, 32, 4096, 4096])
-    y_case3 = np.random.random(size=[1, 32, 4096, 192])
-    dout_case3 = np.random.random(size=[1, 32, 4096, 4096])
+    x_case3 = np.random.random(size=[1, 32, 4096, 4096]).astype("float32")
+    y_case3 = np.random.random(size=[1, 32, 4096, 192]).astype("float32")
+    dout_case3 = np.random.random(size=[1, 32, 4096, 192]).astype("float32")
 
     np.savez("./inputs_case1.npz", x = x_case1, y = y_case1, dout = dout_case1)
     np.savez("./inputs_case2.npz", x = x_case2, y = y_case2, dout = dout_case2)
@@ -33,11 +33,16 @@ class TestMatmulDevelopCase1_FP32(unittest.TestCase):
         self.init_np_inputs_and_dout()
         x_torch, y_torch, dout_torch = self.gen_torch_inputs_and_dout()
         out_torch, out_grads_torch = self.cal_torch_res(x_torch, y_torch, self.transpose_x, self.transpose_y, dout_torch)
+        del x_torch 
+        del y_torch 
+        del dout_torch 
         self.out_torch = out_torch.cpu().detach().numpy()
         self.out_grads_torch = map_structure(
                                 lambda x: x.cpu().numpy(),
                                 out_grads_torch,
                             )
+        del out_torch, out_grads_torch
+        torch.cuda.empty_cache()
 
     def init_params(self):
         self.np_input_dir = "./inputs_case1.npz"
@@ -58,14 +63,10 @@ class TestMatmulDevelopCase1_FP32(unittest.TestCase):
         self.np_y = np_inputs_array["y"]
         self.np_dout = np_inputs_array["dout"]
         # convert np array dtype
-        if self.dtype == "bfloat16":
-            self.np_x = self.np_x.astype("float32")
-            self.np_y = self.np_y.astype("float32")
-            self.np_dout = self.np_dout.astype("float32")
-        else:
-            self.np_x = self.np_x.astype(self.dtype)
-            self.np_y = self.np_y.astype(self.dtype)
-            self.np_dout = self.np_dout.astype(self.dtype)
+        if self.dtype == "float16":
+            self.np_x = self.np_x.astype("float16")
+            self.np_y = self.np_y.astype("float16")
+            self.np_dout = self.np_dout.astype("float16")
     
     def gen_torch_inputs_and_dout(self):
         x_torch = torch.tensor(
@@ -164,7 +165,7 @@ class TestMatmulDevelopCase1_FP32(unittest.TestCase):
             dout_t = paddle.cast(dout, dtype="uint16")
         out = paddle.matmul(x_t, y_t, transpose_x, transpose_y)
         out_grads = paddle.grad(
-            [out], [x, y], grad_outputs=[dout_t], retain_graph=True
+            [out], [x, y], grad_outputs=[dout_t]
         )
         if self.dtype == "bfloat16":
             out = paddle.cast(out, dtype="float32")
@@ -188,7 +189,13 @@ class TestMatmulDevelopCase1_FP32(unittest.TestCase):
 
     def test_eager_accuracy(self):
         x_eager, y_eager, dout_eager = self.gen_eager_inputs_and_dout()
+        print(paddle.device.cuda.memory_allocated(paddle.CUDAPlace(0)))
+
         out_eager, out_grads_eager = self.cal_eager_res(x_eager, y_eager, self.transpose_x, self.transpose_y, dout_eager)
+        del x_eager
+        del y_eager 
+        del dout_eager
+        print(paddle.device.cuda.memory_allocated(paddle.CUDAPlace(0)))
         out_eager = out_eager.numpy()
         out_grads_eager = map_structure(
                                 lambda x: x.numpy(),

test_matmul/test_matmul_incubate.py
@@ -3,9 +3,171 @@ import paddle
 import torch
 import unittest
 from paddle.fluid.layers.utils import map_structure
-from test_matmul_develop import TestMatmulDevelopCase1_FP32
+import sys
+sys.path.append("..")
+from utils import TOLERANCE, convert_dtype_to_torch_type
 
-class TestMatmulIncubateCase1_FP32(TestMatmulDevelopCase1_FP32):
+class TestMatmulIncubateCase1_FP32(unittest.TestCase):
+    def setUp(self):
+        self.init_params()
+        self.init_threshold()
+        self.init_np_inputs_and_dout()
+        x_torch, y_torch, dout_torch = self.gen_torch_inputs_and_dout()
+        out_torch, out_grads_torch = self.cal_torch_res(x_torch, y_torch, self.transpose_x, self.transpose_y, dout_torch)
+        del x_torch 
+        del y_torch 
+        del dout_torch 
+        self.out_torch = out_torch.cpu().detach().numpy()
+        self.out_grads_torch = map_structure(
+                                lambda x: x.cpu().numpy(),
+                                out_grads_torch,
+                            )
+        del out_torch, out_grads_torch
+        torch.cuda.empty_cache()
+
+    def init_params(self):
+        self.np_input_dir = "./inputss_case1.npz"
+        self.transpose_x = False
+        self.transpose_y = True
+        self.dtype = "float32"
+        self.save_static_res_path = "./static_develop_res_case1_fp32.npz"
+        self.save_eager_res_path = "./eager_develop_res_case1_fp32.npz"
+    
+    def init_threshold(self):
+        self.atol = TOLERANCE[self.dtype]["atol"]
+        self.rtol = TOLERANCE[self.dtype]["rtol"]
+
+    def init_np_inputs_and_dout(self):
+        np_inputs_array = np.load(self.np_input_dir)
+        # get np array from npz file
+        self.np_x = np_inputs_array["x"]
+        self.np_y = np_inputs_array["y"]
+        self.np_dout = np_inputs_array["dout"]
+        # convert np array dtype
+        if self.dtype == "float16":
+            self.np_x = self.np_x.astype("float16")
+            self.np_y = self.np_y.astype("float16")
+            self.np_dout = self.np_dout.astype("float16")
+    
+    def gen_torch_inputs_and_dout(self):
+        x_torch = torch.tensor(
+            self.np_x,
+            device='cuda',
+            dtype=convert_dtype_to_torch_type(self.dtype)
+            if self.dtype != 'bfloat16'
+            else torch.float32,
+            requires_grad=True,
+        )
+        y_torch = torch.tensor(
+            self.np_y,
+            device='cuda',
+            dtype=convert_dtype_to_torch_type(self.dtype)
+            if self.dtype != 'bfloat16'
+            else torch.float32,
+            requires_grad=True,
+        )
+        dout_torch = torch.tensor(
+            self.np_dout,
+            device='cuda',
+            dtype=convert_dtype_to_torch_type(self.dtype)
+            if self.dtype != 'bfloat16'
+            else torch.float32,
+            requires_grad=True,
+        )
+        return x_torch, y_torch, dout_torch
+    
+    def gen_eager_inputs_and_dout(self):
+        x_eager = paddle.to_tensor(
+            self.np_x,
+            dtype=self.dtype if self.dtype != 'bfloat16' else "float32",
+            place="gpu",
+        )
+        x_eager.stop_gradient = False
+        y_eager = paddle.to_tensor(
+            self.np_y,
+            dtype=self.dtype if self.dtype != 'bfloat16' else "float32",
+            place="gpu",
+        )
+        y_eager.stop_gradient = False
+        dout_eager = paddle.to_tensor(
+            self.np_dout,
+            dtype=self.dtype if self.dtype != 'bfloat16' else "float32",
+            place="gpu",
+        )
+        dout_eager.stop_gradient = False
+        return x_eager, y_eager, dout_eager
+
+    def gen_static_inputs_and_dout(self):
+        x_static = paddle.static.data(
+            'x',
+            shape=self.np_x.shape,
+            dtype=self.dtype if self.dtype != "bfloat16" else "float32",
+        )
+        x_static.stop_gradient = False
+        y_static = paddle.static.data(
+            'y',
+            shape=self.np_y.shape,
+            dtype=self.dtype if self.dtype != "bfloat16" else "float32",
+        )
+        y_static.stop_gradient = False
+        dout_static = paddle.static.data(
+            'dout',
+            shape=self.np_dout.shape,
+            dtype=self.dtype if self.dtype != "bfloat16" else "float32",
+        )
+        dout_static.stop_gradient = False
+        return x_static, y_static, dout_static
+
+    def cal_torch_res(self, x, y, transpose_x, transpose_y, dout):
+        x_t = x
+        y_t = y
+        dout_t = dout
+        if self.dtype == "bfloat16":
+            x_t = x.to(dtype=torch.bfloat16)
+            y_t = y.to(dtype=torch.bfloat16)
+            dout_t = dout.to(dtype=torch.bfloat16)
+        if transpose_x:
+            x_t = torch.transpose(x_t, -1, -2)
+        if transpose_y:
+            y_t = torch.transpose(y_t, -1, -2)
+        out = torch.matmul(x_t, y_t)
+        out_grads = torch.autograd.grad([out], [x, y], grad_outputs=[dout_t])
+        if self.dtype == "bfloat16":
+            out = out.to(dtype=torch.float32)
+        return out, out_grads
+
+    def cal_eager_res(self, x, y, transpose_x, transpose_y, dout):
+        x_t = x
+        y_t = y
+        dout_t = dout
+        if self.dtype == "bfloat16":
+            x_t = paddle.cast(x, dtype="uint16")
+            y_t = paddle.cast(y, dtype="uint16")
+            dout_t = paddle.cast(dout, dtype="uint16")
+        out = paddle.matmul(x_t, y_t, transpose_x, transpose_y)
+        out_grads = paddle.grad(
+            [out], [x, y], grad_outputs=[dout_t], retain_graph=True
+        )
+        if self.dtype == "bfloat16":
+            out = paddle.cast(out, dtype="float32")
+        return out, out_grads
+
+    def cal_static_res(self, x, y, transpose_x, transpose_y, dout):
+        x_t = x
+        y_t = y
+        dout_t = dout
+        if self.dtype == "bfloat16":
+            x_t = paddle.cast(x, dtype="uint16")
+            y_t = paddle.cast(y, dtype="uint16")
+            dout_t = paddle.cast(dout, dtype="uint16")
+        out = paddle.matmul(x_t, y_t, transpose_x, transpose_y)
+        out_grads = paddle.static.gradients(
+            [out], [x, y], target_gradients=[dout_t]
+        )
+        if self.dtype == "bfloat16":
+            out = paddle.cast(out, dtype="float32")
+        return out, out_grads
+
     def test_eager_accuracy(self):
         # get develop eager res
         develop_res_array = np.load(self.save_eager_res_path)
@@ -93,9 +255,90 @@ class TestMatmulIncubateCase1_FP32(TestMatmulDevelopCase1_FP32):
                 % self.dtype,
             )
 
+    def test_eager_stability(self):
+        x_eager, y_eager, dout_eager = self.gen_eager_inputs_and_dout()
+        out_eager_baseline, out_grads_eager_baseline = self.cal_eager_res(x_eager, y_eager, self.transpose_x, self.transpose_y, dout_eager)
+        out_eager_baseline = out_eager_baseline.numpy()
+        out_grads_eager_baseline = map_structure(
+                                lambda x: x.numpy(),
+                                out_grads_eager_baseline,
+                            )
+        for i in range(50):
+            out_eager, out_grads_eager = self.cal_eager_res(x_eager, y_eager, self.transpose_x, self.transpose_y, dout_eager)
+            out_eager = out_eager.numpy()
+            out_grads_eager = map_structure(
+                                    lambda x: x.numpy(),
+                                    out_grads_eager,
+                                )
+            np.testing.assert_equal(
+                out_eager,
+                out_eager_baseline,
+                err_msg=(
+                    'Develop: paddle.matmul eager forward is unstable in %s dtype'
+                )
+                % self.dtype,
+            )
+            for idx in range(len(out_grads_eager)):
+                np.testing.assert_equal(
+                    out_grads_eager[idx],
+                    out_grads_eager_baseline[idx],
+                    err_msg=(
+                        'Develop: paddle.matmul eager grad is unstable in %s dtype'
+                    )
+                    % self.dtype,
+                )
+
+    def test_static_stability(self):
+        with paddle.fluid.framework._dygraph_guard(None):
+            mp, sp = paddle.static.Program(), paddle.static.Program()
+            with paddle.static.program_guard(mp, sp):
+                x_static, y_static, dout_static = self.gen_static_inputs_and_dout()
+                (out_static_pg, out_grads_static_pg) = self.cal_static_res(
+                    x_static,
+                    y_static,
+                    self.transpose_x,
+                    self.transpose_y,
+                    dout_static,
+                )
+            exe = paddle.static.Executor(
+                place=paddle.CUDAPlace(0)
+            )
+            exe.run(sp)
+            out = exe.run(
+                mp,
+                feed={"x": self.np_x, "y": self.np_y, "dout": self.np_dout},
+                fetch_list=[out_static_pg] + out_grads_static_pg,
+            )
+            out_static_baseline, out_grads_static_baseline = out[0], out[1:]
+            for i in range(50):
+                out = exe.run(
+                    mp,
+                    feed={"x": self.np_x, "y": self.np_y, "dout": self.np_dout},
+                    fetch_list=[out_static_pg] + out_grads_static_pg,
+                )
+                out_static, out_grads_static = out[0], out[1:]
+                np.testing.assert_equal(
+                    out_static,
+                    out_static_baseline,
+                    err_msg=(
+                        'Develop: paddle.matmul static forward is unstable in %s dtype'
+                    )
+                    % self.dtype,
+                )
+                for idx in range(len(out_grads_static)):
+                    np.testing.assert_equal(
+                        out_grads_static[idx],
+                        out_grads_static_baseline[idx],
+                        err_msg=(
+                            'Develop: paddle.matmul static grad is unstable in %s dtype'
+                        )
+                        % self.dtype,
+                    )
+
+
 class TestMatmulIncubateCase1_FP16(TestMatmulIncubateCase1_FP32):
     def init_params(self):
-        self.np_input_dir = "./input_case1.npz"
+        self.np_input_dir = "./inputs_case1.npz"
         self.transpose_x = False
         self.transpose_y = True
         self.dtype = "float16"
@@ -104,7 +347,7 @@ class TestMatmulIncubateCase1_FP16(TestMatmulIncubateCase1_FP32):
 
 class TestMatmulIncubateCase1_BFP16(TestMatmulIncubateCase1_FP32):
     def init_params(self):
-        self.np_input_dir = "./input_case1.npz"
+        self.np_input_dir = "./inputs_case1.npz"
         self.transpose_x = False
         self.transpose_y = True
         self.dtype = "bfloat16"
@@ -113,7 +356,7 @@ class TestMatmulIncubateCase1_BFP16(TestMatmulIncubateCase1_FP32):
 
 class TestMatmulIncubateCase2_FP32(TestMatmulIncubateCase1_FP32):
     def init_params(self):
-        self.np_input_dir = "./input_case2.npz"
+        self.np_input_dir = "./inputs_case2.npz"
         self.transpose_x = False
         self.transpose_y = False
         self.dtype = "float32"
@@ -122,7 +365,7 @@ class TestMatmulIncubateCase2_FP32(TestMatmulIncubateCase1_FP32):
 
 class TestMatmulIncubateCase2_FP16(TestMatmulIncubateCase1_FP32):
     def init_params(self):
-        self.np_input_dir = "./input_case2.npz"
+        self.np_input_dir = "./inputs_case2.npz"
         self.transpose_x = False
         self.transpose_y = False
         self.dtype = "float16"
@@ -131,7 +374,7 @@ class TestMatmulIncubateCase2_FP16(TestMatmulIncubateCase1_FP32):
 
 class TestMatmulIncubateCase2_BFP16(TestMatmulIncubateCase1_FP32):
     def init_params(self):
-        self.np_input_dir = "./input_case2.npz"
+        self.np_input_dir = "./inputs_case2.npz"
         self.transpose_x = False
         self.transpose_y = False
         self.dtype = "bfloat16"
@@ -140,7 +383,7 @@ class TestMatmulIncubateCase2_BFP16(TestMatmulIncubateCase1_FP32):
 
 class TestMatmulIncubateCase3_FP32(TestMatmulIncubateCase1_FP32):
     def init_params(self):
-        self.np_input_dir = "./input_case3.npz"
+        self.np_input_dir = "./inputs_case3.npz"
         self.transpose_x = False
         self.transpose_y = False
         self.dtype = "float32"
@@ -149,7 +392,7 @@ class TestMatmulIncubateCase3_FP32(TestMatmulIncubateCase1_FP32):
 
 class TestMatmulIncubateCase3_FP16(TestMatmulIncubateCase1_FP32):
     def init_params(self):
-        self.np_input_dir = "./input_case3.npz"
+        self.np_input_dir = "./inputs_case3.npz"
         self.transpose_x = False
         self.transpose_y = False
         self.dtype = "float16"
@@ -158,7 +401,7 @@ class TestMatmulIncubateCase3_FP16(TestMatmulIncubateCase1_FP32):
 
 class TestMatmulIncubateCase3_BFP16(TestMatmulIncubateCase1_FP32):
     def init_params(self):
-        self.np_input_dir = "./input_case3.npz"
+        self.np_input_dir = "./inputs_case3.npz"
         self.transpose_x = False
         self.transpose_y = False
         self.dtype = "bfloat16"