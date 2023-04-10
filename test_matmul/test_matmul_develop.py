fix memory overflow

test_matmul/test_matmul_develop.py
@@ -189,24 +189,25 @@ class TestMatmulDevelopCase1_FP32(unittest.TestCase):
 
     def test_eager_accuracy(self):
         x_eager, y_eager, dout_eager = self.gen_eager_inputs_and_dout()
-        print(paddle.device.cuda.memory_allocated(paddle.CUDAPlace(0)))
-
         out_eager, out_grads_eager = self.cal_eager_res(x_eager, y_eager, self.transpose_x, self.transpose_y, dout_eager)
         del x_eager
         del y_eager 
         del dout_eager
-        print(paddle.device.cuda.memory_allocated(paddle.CUDAPlace(0)))
-        out_eager = out_eager.numpy()
-        out_grads_eager = map_structure(
+        paddle.device.cuda.empty_cache()
+        out_eager_np = out_eager.numpy()
+        out_grads_eager_np = map_structure(
                                 lambda x: x.numpy(),
                                 out_grads_eager,
                             )
+        del out_eager
+        del out_grads_eager
+        paddle.device.cuda.empty_cache()
         # save eager res for test_matmul_incubate
-        np.savez(self.save_eager_res_path, out_eager=out_eager, out_grads_eager_0=out_grads_eager[0], out_grads_eager_1=out_grads_eager[1])
+        np.savez(self.save_eager_res_path, out_eager=out_eager_np, out_grads_eager_0=out_grads_eager_np[0], out_grads_eager_1=out_grads_eager_np[1])
         
         # compare eager res with torch
         np.testing.assert_allclose(
-            out_eager,
+            out_eager_np,
             self.out_torch,
             self.atol,
             self.rtol,
@@ -215,9 +216,9 @@ class TestMatmulDevelopCase1_FP32(unittest.TestCase):
             )
             % self.dtype,
         )
-        for idx in range(len(out_grads_eager)):
+        for idx in range(len(out_grads_eager_np)):
             np.testing.assert_allclose(
-                out_grads_eager[idx],
+                out_grads_eager_np[idx],
                 self.out_grads_torch[idx],
                 self.atol,
                 self.rtol,
@@ -279,11 +280,15 @@ class TestMatmulDevelopCase1_FP32(unittest.TestCase):
     def test_eager_stability(self):
         x_eager, y_eager, dout_eager = self.gen_eager_inputs_and_dout()
         out_eager_baseline, out_grads_eager_baseline = self.cal_eager_res(x_eager, y_eager, self.transpose_x, self.transpose_y, dout_eager)
-        out_eager_baseline = out_eager_baseline.numpy()
-        out_grads_eager_baseline = map_structure(
+        out_eager_baseline_np = out_eager_baseline.numpy()
+        out_grads_eager_baseline_np = map_structure(
                                 lambda x: x.numpy(),
                                 out_grads_eager_baseline,
                             )
+        del out_eager_baseline
+        del out_grads_eager_baseline
+        paddle.device.cuda.empty_cache()
+
         for i in range(50):
             out_eager, out_grads_eager = self.cal_eager_res(x_eager, y_eager, self.transpose_x, self.transpose_y, dout_eager)
             out_eager = out_eager.numpy()
@@ -293,7 +298,7 @@ class TestMatmulDevelopCase1_FP32(unittest.TestCase):
                                 )
             np.testing.assert_equal(
                 out_eager,
-                out_eager_baseline,
+                out_eager_baseline_np,
                 err_msg=(
                     'Develop: paddle.matmul eager forward is unstable in %s dtype'
                 )
@@ -302,7 +307,7 @@ class TestMatmulDevelopCase1_FP32(unittest.TestCase):
             for idx in range(len(out_grads_eager)):
                 np.testing.assert_equal(
                     out_grads_eager[idx],
-                    out_grads_eager_baseline[idx],
+                    out_grads_eager_baseline_np[idx],
                     err_msg=(
                         'Develop: paddle.matmul eager grad is unstable in %s dtype'
                     )

test_matmul/test_matmul_incubate.py
@@ -179,24 +179,30 @@ class TestMatmulIncubateCase1_FP32(unittest.TestCase):
         # calculate incubate eager res
         x_eager, y_eager, dout_eager = self.gen_eager_inputs_and_dout()
         out_eager, out_grads_eager = self.cal_eager_res(x_eager, y_eager, self.transpose_x, self.transpose_y, dout_eager)
-        out_eager = out_eager.numpy()
-        out_grads_eager = map_structure(
+        del x_eager
+        del y_eager 
+        del dout_eager
+        paddle.device.cuda.empty_cache()
+        out_eager_np = out_eager.numpy()
+        out_grads_eager_np = map_structure(
                                 lambda x: x.numpy(),
                                 out_grads_eager,
                             )
-        
+        del out_eager
+        del out_grads_eager
+        paddle.device.cuda.empty_cache()
         # compare incubate eager res with develop eager res
         np.testing.assert_equal(
-            out_eager,
+            out_eager_np,
             out_eager_develop,
             err_msg=(
                 'Incubate: compare matmul incubate eager forward res with develop eager forward res failed in %s dtype'
             )
             % self.dtype,
         )
-        for idx in range(len(out_grads_eager)):
+        for idx in range(len(out_grads_eager_np)):
             np.testing.assert_equal(
-                out_grads_eager[idx],
+                out_grads_eager_np[idx],
                 out_eager_grads_develop,
             err_msg=(
                 'Incubate: compare matmul incubate eager grad res with develop eager grad res failed in %s dtype'
@@ -258,11 +264,15 @@ class TestMatmulIncubateCase1_FP32(unittest.TestCase):
     def test_eager_stability(self):
         x_eager, y_eager, dout_eager = self.gen_eager_inputs_and_dout()
         out_eager_baseline, out_grads_eager_baseline = self.cal_eager_res(x_eager, y_eager, self.transpose_x, self.transpose_y, dout_eager)
-        out_eager_baseline = out_eager_baseline.numpy()
-        out_grads_eager_baseline = map_structure(
+        out_eager_baseline_np = out_eager_baseline.numpy()
+        out_grads_eager_baseline_np = map_structure(
                                 lambda x: x.numpy(),
                                 out_grads_eager_baseline,
                             )
+        del out_eager_baseline
+        del out_grads_eager_baseline
+        paddle.device.cuda.empty_cache()
+
         for i in range(50):
             out_eager, out_grads_eager = self.cal_eager_res(x_eager, y_eager, self.transpose_x, self.transpose_y, dout_eager)
             out_eager = out_eager.numpy()
@@ -272,7 +282,7 @@ class TestMatmulIncubateCase1_FP32(unittest.TestCase):
                                 )
             np.testing.assert_equal(
                 out_eager,
-                out_eager_baseline,
+                out_eager_baseline_np,
                 err_msg=(
                     'Develop: paddle.matmul eager forward is unstable in %s dtype'
                 )
@@ -281,7 +291,7 @@ class TestMatmulIncubateCase1_FP32(unittest.TestCase):
             for idx in range(len(out_grads_eager)):
                 np.testing.assert_equal(
                     out_grads_eager[idx],
-                    out_grads_eager_baseline[idx],
+                    out_grads_eager_baseline_np[idx],
                     err_msg=(
                         'Develop: paddle.matmul eager grad is unstable in %s dtype'
                     )
@@ -342,8 +352,8 @@ class TestMatmulIncubateCase1_FP16(TestMatmulIncubateCase1_FP32):
         self.transpose_x = False
         self.transpose_y = True
         self.dtype = "float16"
-        self.save_static_res_path = "./static_develop_res_case1_fp16.npy"
-        self.save_eager_res_path = "./eager_develop_res_case1_fp16.npy"
+        self.save_static_res_path = "./static_develop_res_case1_fp16.npz"
+        self.save_eager_res_path = "./eager_develop_res_case1_fp16.npz"
 
 class TestMatmulIncubateCase1_BFP16(TestMatmulIncubateCase1_FP32):
     def init_params(self):
@@ -351,8 +361,8 @@ class TestMatmulIncubateCase1_BFP16(TestMatmulIncubateCase1_FP32):
         self.transpose_x = False
         self.transpose_y = True
         self.dtype = "bfloat16"
-        self.save_static_res_path = "./static_develop_res_case1_bfp16.npy"
-        self.save_eager_res_path = "./eager_develop_res_case1_bfp16.npy"
+        self.save_static_res_path = "./static_develop_res_case1_bfp16.npz"
+        self.save_eager_res_path = "./eager_develop_res_case1_bfp16.npz"
 
 class TestMatmulIncubateCase2_FP32(TestMatmulIncubateCase1_FP32):
     def init_params(self):
@@ -360,8 +370,8 @@ class TestMatmulIncubateCase2_FP32(TestMatmulIncubateCase1_FP32):
         self.transpose_x = False
         self.transpose_y = False
         self.dtype = "float32"
-        self.save_static_res_path = "./static_develop_res_case2_fp32.npy"
-        self.save_eager_res_path = "./eager_develop_res_case2_fp32.npy"
+        self.save_static_res_path = "./static_develop_res_case2_fp32.npz"
+        self.save_eager_res_path = "./eager_develop_res_case2_fp32.npz"
 
 class TestMatmulIncubateCase2_FP16(TestMatmulIncubateCase1_FP32):
     def init_params(self):
@@ -369,8 +379,8 @@ class TestMatmulIncubateCase2_FP16(TestMatmulIncubateCase1_FP32):
         self.transpose_x = False
         self.transpose_y = False
         self.dtype = "float16"
-        self.save_static_res_path = "./static_develop_res_case2_fp16.npy"
-        self.save_eager_res_path = "./eager_develop_res_case2_fp16.npy"
+        self.save_static_res_path = "./static_develop_res_case2_fp16.npz"
+        self.save_eager_res_path = "./eager_develop_res_case2_fp16.npz"
 
 class TestMatmulIncubateCase2_BFP16(TestMatmulIncubateCase1_FP32):
     def init_params(self):
@@ -378,8 +388,8 @@ class TestMatmulIncubateCase2_BFP16(TestMatmulIncubateCase1_FP32):
         self.transpose_x = False
         self.transpose_y = False
         self.dtype = "bfloat16"
-        self.save_static_res_path = "./static_develop_res_case2_bfp16.npy"
-        self.save_eager_res_path = "./eager_develop_res_case2_bfp16.npy"
+        self.save_static_res_path = "./static_develop_res_case2_bfp16.npz"
+        self.save_eager_res_path = "./eager_develop_res_case2_bfp16.npz"
 
 class TestMatmulIncubateCase3_FP32(TestMatmulIncubateCase1_FP32):
     def init_params(self):
@@ -387,8 +397,8 @@ class TestMatmulIncubateCase3_FP32(TestMatmulIncubateCase1_FP32):
         self.transpose_x = False
         self.transpose_y = False
         self.dtype = "float32"
-        self.save_static_res_path = "./static_develop_res_case3_fp32.npy"
-        self.save_eager_res_path = "./eager_develop_res_case3_fp32.npy"
+        self.save_static_res_path = "./static_develop_res_case3_fp32.npz"
+        self.save_eager_res_path = "./eager_develop_res_case3_fp32.npz"
 
 class TestMatmulIncubateCase3_FP16(TestMatmulIncubateCase1_FP32):
     def init_params(self):
@@ -396,8 +406,8 @@ class TestMatmulIncubateCase3_FP16(TestMatmulIncubateCase1_FP32):
         self.transpose_x = False
         self.transpose_y = False
         self.dtype = "float16"
-        self.save_static_res_path = "./static_develop_res_case3_fp16.npy"
-        self.save_eager_res_path = "./eager_develop_res_case3_fp16.npy"
+        self.save_static_res_path = "./static_develop_res_case3_fp16.npz"
+        self.save_eager_res_path = "./eager_develop_res_case3_fp16.npz"
 
 class TestMatmulIncubateCase3_BFP16(TestMatmulIncubateCase1_FP32):
     def init_params(self):
@@ -405,8 +415,8 @@ class TestMatmulIncubateCase3_BFP16(TestMatmulIncubateCase1_FP32):
         self.transpose_x = False
         self.transpose_y = False
         self.dtype = "bfloat16"
-        self.save_static_res_path = "./static_develop_res_case3_bfp16.npy"
-        self.save_eager_res_path = "./eager_develop_res_case3_bfp16.npy"
+        self.save_static_res_path = "./static_develop_res_case3_bfp16.npz"
+        self.save_eager_res_path = "./eager_develop_res_case3_bfp16.npz"
 
 if __name__ == '__main__':
     unittest.main()