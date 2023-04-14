a7ff7a6cf9b5ce9be146287109d8c2bf6dc5471c
yuehuayingxueluo <867460659@qq.com>
2023-04-14 06:12:17 +0000

    rm dtype = dtype

--- test_vocab_parallel_embedding/get_torch_result.py
+++ test_vocab_parallel_embedding/get_torch_result.py
@@ -80,7 +80,6 @@ prepare_data.generate_np_inputs_and_dout()
 for dtype_id, dtype in enumerate(dtype_list):
 
     np_input_dir = "./inputs_case{id_b}.npz".format(id_b=(dtype_id + 1))
-    dtype = dtype
     torch_dir = "./torch_out_{dtype}.npz".format(dtype=dtype)
 
     test_torch = TestTorch('cuda', np_input_dir, dtype, torch_dir)
