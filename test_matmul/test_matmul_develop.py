09d056b481c55fa9eeed061b93e08b8ae64ee102
Charles-hit <wanghao107@baidu.com>
2023-04-21 06:07:51 +0000

    modify matmul input

--- test_matmul/test_matmul_develop.py
+++ test_matmul/test_matmul_develop.py
@@ -9,13 +9,13 @@ from utils import TOLERANCE, convert_dtype_to_torch_type
 
 
 def generate_np_inputs_and_dout():
-    x_case1 = np.random.random(size=[1, 32, 4096, 192]).astype("float32")
-    y_case1 = np.random.random(size=[1, 32, 4096, 192]).astype("float32")
-    dout_case1 = np.random.random(size=[1, 32, 4096, 4096]).astype("float32")
+    x_case1 = np.random.random(size=[1, 32, 4096, 192]).astype("float32")-0.5
+    y_case1 = np.random.random(size=[1, 32, 4096, 192]).astype("float32")-0.5
+    dout_case1 = np.random.random(size=[1, 32, 4096, 4096]).astype("float32")-0.5
 
-    x_case2 = np.random.random(size=[1, 32, 4096, 4096]).astype("float32")
-    y_case2 = np.random.random(size=[1, 32, 4096, 192]).astype("float32")
-    dout_case2 = np.random.random(size=[1, 32, 4096, 192]).astype("float32")
+    x_case2 = np.random.random(size=[1, 32, 4096, 4096]).astype("float32")-0.5
+    y_case2 = np.random.random(size=[1, 32, 4096, 192]).astype("float32")-0.5
+    dout_case2 = np.random.random(size=[1, 32, 4096, 192]).astype("float32")-0.5
 
     np.savez("./inputs_case1.npz", x = x_case1, y = y_case1, dout = dout_case1)
     np.savez("./inputs_case2.npz", x = x_case2, y = y_case2, dout = dout_case2)
