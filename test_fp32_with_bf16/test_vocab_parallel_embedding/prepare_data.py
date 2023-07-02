2783aabffb2e7219f13e8bc5c0b7fc73d6a27fbd
yuehuayingxueluo <867460659@qq.com>
2023-07-02 20:40:25 +0800

    add fp32_bf16 test for fc, silu and vocab_parallel_embedding

---
+++ test_fp32_with_bf16/test_vocab_parallel_embedding/prepare_data.py
@@ -0,0 +1,16 @@
+import numpy as np
+import init_config_class
+
+def generate_np_inputs_and_dout():
+    np.random.seed(0)
+
+    dim_1 = init_config_class.dim_1[0]
+    dim_2 = init_config_class.dim_2[0]
+    dim_3 = init_config_class.dim_3[0]
+    x_case1 = np.random.randint(low=0, high=dim_1, size=[1, dim_2]).astype("int64")
+    table_case1 = np.random.random(size=[dim_1, dim_3]).astype("float32") - 0.5 
+    dout_case1 = np.random.random(size=[1, dim_2, dim_3]).astype("float32") - 0.5
+
+    np.savez("./inputs_case1.npz", x = x_case1, table = table_case1, dout = dout_case1)
+
+generate_np_inputs_and_dout()
---
+++ test_fp32_with_bf16/test_vocab_parallel_embedding/test_vocab_parallel_embedding.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -ex
+
+card_num=$1
+version=$2
+log_dir=$3
+
+export NVIDIA_TF32_OVERRIDE=0
+
+case $card_num in 
+    2 ) export CUDA_VISIBLE_DEVICES=0,1 ;;
+    4 ) export CUDA_VISIBLE_DEVICES=0,1,2,3 ;;
+    8 ) export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 ;;
+    * )  
+        echo '请输入正确的卡数'  
+        exit 
+    ;;
+esac
+
+#please run "bash test_vocab_parallel_embedding.sh * develop" first
+
+if [ "$version" == 'develop' ]; then
+    rm -rf *.npz
+    python prepare_data.py
+    python -m paddle.distributed.launch --log_dir $log_dir get_and_test_paddle_result.py
+else
+   echo "请输入develop或者incubate"
+fi
